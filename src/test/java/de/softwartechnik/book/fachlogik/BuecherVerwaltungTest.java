137e1296228d60a30799f82a9cc4785cc006bb17
Felix Klauke <info@felix-klauke.de>
2019-12-02 12:48:37 +0100

    Add test infrastructure

---
+++ src/test/java/de/softwartechnik/book/fachlogik/BuecherVerwaltungTest.java
@@ -0,0 +1,33 @@
+package de.softwartechnik.book.fachlogik;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BuecherVerwaltungTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @Test
+  void add() {
+  }
+
+  @Test
+  void getBuch() {
+  }
+
+  @Test
+  void getBuchliste() {
+  }
+
+  @Test
+  void laden() {
+  }
+
+  @Test
+  void speichern() {
+  }
+}
